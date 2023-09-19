6c0228ca8c276e348bfadf38a8ebd8b74ebea2cc
Polina <shapovalova2006p@gmail.com>
2023-09-19 23:02:58 +1000

    fixed znack in switch, add negNumbers, fraction

--- script.js
+++ script.js
@@ -2,12 +2,24 @@ const buttons = document.querySelector('.buttons')
 const vrt = document.querySelector('.vrt')
 const vrb = document.querySelector('.vrb')
 const deleteBtn = document.querySelector('.delete')
+
+
 let a = [];
 let b = [];
 let znack = null;
-const numbers = (".0123456789").split("")
+const numbersPoint = (".0123456789").split("")
 const znacks = ("-+/*").split("") 
 let elements = ("789/456*123-.0=+").split("")
+
+const outputAndCheckPoint = (array, value) => {
+  if (!array.length && value == '.'){
+    array.push(0, value)
+    vrb.textContent = array.join("") 
+  }else {
+    array.push(value)
+    vrb.textContent = array.join("")
+  }
+}
 elements.forEach((el) => {
   const button = document.createElement('button')
   button.textContent = el
@@ -15,7 +27,7 @@ elements.forEach((el) => {
   button.id = el
   
   button.addEventListener("click", (e) => {
-    if (znack == '='){
+    if (znack == '=' || a == 'ERROR'){
       vrb.textContent = ''
       a = []
       znack = null
@@ -23,6 +35,7 @@ elements.forEach((el) => {
     if (a.length != 0 && b.length != 0 && (znacks.includes(e.target.id) || (znack != null && e.target.id == '='))){
       if (Array.isArray(a)){a = +a.join('');}
       b = +b.join('');
+      vrt.textContent = a + znack + b;
       switch (znack) {
         case '+' :
           a = a + b
@@ -37,34 +50,39 @@ elements.forEach((el) => {
           if (b != 0){
           a = a / b
           } else {
-            alert('ERROR')
-            a = [];
+            a = 'ERROR';
             znack = null;
           }
           break;
       }
+      a = +a.toFixed(5)
       b = []
-      znack = null
-      if (e.target.id == '=') {
+      znack = e.target.id
+      if (znack == '=') {
       znack = '='
       vrb.textContent = a;
       vrt.textContent = ""
       } else {
-        vrb.textContent = a;
+        if (a == 'ERROR') {
+          vrb.textContent = 0;
+        } else {
+          vrb.textContent = a;
+        }
       }
     // a
-    } else if (a.length == 0 || znack == null && numbers.includes(e.target.id)){
-      console.log(e.target.id)
-      a.push(e.target.id)
-      vrb.textContent = a.join("") 
+    } else if ((znack == null && numbersPoint.includes(e.target.id)) ||
+    (a.length == 0 && e.target.id == '-') || (e.target.id == '.' && 
+    !a.filter((el) => el == ".").reduce((total, next) => total + 1, 0))){
+      outputAndCheckPoint(a, e.target.id)
       // znack
     } else if (a.length != 0 && znacks.includes(e.target.id)){
       znack = e.target.id
       vrt.textContent = a.join("") + znack
       // b
-    } else if (a.length != 0 && znack != null && numbers.includes(e.target.id)) {
-      b.push(e.target.id)
-      vrb.textContent = b.join('')
+    } else if ( a.length != 0 && znack != null && (numbersPoint.includes(e.target.id) ||
+    (b.length == 0 && e.target.id == '-') || (e.target.id == '.' && 
+    !b.filter((el) => el == ".").reduce((total, next) => total + 1, 0))) ){
+      outputAndCheckPoint(b, e.target.id)
     }
   }
   ) 
